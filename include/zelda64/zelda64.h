c76e9221625e41591b90383700aa736eb05ad820
Jesse Gerard Brands <jesse@jgbrands.nl>
2026-08-01 16:10:18 +0200

    Add copyright and license disclaimer to source files

--- include/zelda64/zelda64.h
+++ include/zelda64/zelda64.h
@@ -1,3 +1,23 @@
+/*
+ * zelda64.h: Nintendo 64 Zelda ROM manipulation library
+ * Copyright (C) 2026 Jesse Gerard Brands
+ *
+ * This file is part of libzelda64.
+ *
+ * libzelda64 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * libzelda64 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libzelda64. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef ZELDA64_ZELDA64_H
 #define ZELDA64_ZELDA64_H
 
