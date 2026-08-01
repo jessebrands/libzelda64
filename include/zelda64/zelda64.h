d666aad213a3dc7569e28ebf636ad38fbbd56f87
Jesse Gerard Brands <jesse@jgbrands.nl>
2026-08-01 16:00:17 +0200

    Add ZELDA64_API export macro

--- include/zelda64/zelda64.h
+++ include/zelda64/zelda64.h
@@ -1,4 +1,18 @@
 #ifndef ZELDA64_ZELDA64_H
 #define ZELDA64_ZELDA64_H
 
+#if defined _WIN32 || defined __CYGWIN__
+#  if defined ZELDA64_STATIC
+#    define ZELDA64_API
+#  elif defined ZELDA64_SHARED
+#    define ZELDA64_API __declspec(dllexport)
+#  else
+#    define ZELDA64_API __declspec(dllimport)
+#  endif
+#elif defined __GNUC__ && !defined ZELDA64_STATIC
+#  define ZELDA64_API __attribute__((visibility("default")))
+#else
+#  define ZELDA64_API
+#endif
+
 #endif //ZELDA64_ZELDA64_H
