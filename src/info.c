e8f4028baa20a98f2b53413dcf57a4fee4e5d5ca
Jesse Gerard Brands <jesse@jgbrands.nl>
2026-08-07 17:16:16 +0200

    Add ROM info command

---
+++ src/info.c
@@ -0,0 +1,78 @@
+/*
+ * info.c: Print Nintendo 64 ROM information
+ * Copyright (C) 2026 Jesse Gerard Brands
+ *
+ * This file is part of zelda64.
+ *
+ * zelda64 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * zelda64 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zelda64. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <zelda64/zelda64.h>
+
+#include "actions.h"
+
+enum zelda64_action_result
+action_rom_info(uint8_t const* rom, size_t const rom_size) {
+    // Find the DMADATA location and size.
+    struct zelda64_dmadata_info dmadata_info = {0};
+    enum zelda64_result result = zelda64_find_dmadata(&dmadata_info, rom, rom_size);
+    if (result != ZELDA64_OK) {
+        return (enum zelda64_action_result) result;
+    }
+
+
+    // Allocate a buffer large enough to hold the DMADATA in memory.
+    struct zelda64_dma_entry* entries = calloc(dmadata_info.count, sizeof(*entries));
+    if (entries == NULL) {
+        return ACTION_MEMORY_ERROR;
+    }
+
+    uint8_t const* dmadata = &rom[dmadata_info.offset];
+    size_t const dmadata_size = dmadata_info.size;
+    result = zelda64_read_dmadata(entries, dmadata_info.count, dmadata, dmadata_size);
+    if (result != ZELDA64_OK) {
+        free(entries);
+        return (enum zelda64_action_result) result;
+    }
+
+    // Read the ROM info now that we have the MAKEROM
+    struct zelda64_rom_info rom_info;
+    uint8_t const* makerom = &rom[entries[0].vrom_start];
+    size_t const makerom_size = entries[0].vrom_end - entries[0].vrom_start;
+    result = zelda64_read_rom_info(&rom_info, makerom, makerom_size);
+    if (result != ZELDA64_OK) {
+        free(entries);
+        return (enum zelda64_action_result) result;
+    }
+
+    fprintf(stdout, "%-20s%.20s\n", "title:", rom_info.header.title);
+    fprintf(stdout, "%-20s%.4s\n", "game code:", rom_info.header.game_code);
+    fprintf(stdout, "%-20s%d\n", "version", rom_info.header.version + 1);
+    fprintf(stdout, "%-20s0x%016" PRIX64 "\n", "check code:", rom_info.header.check_code);
+    fprintf(stdout, "%-20s%s\n", "cic:", zelda64_cic_name(rom_info.cic));
+    fprintf(stdout, "%-20s0x%08" PRIX32 "\n", "ipl checksum:", rom_info.ipl_checksum);
+    fprintf(stdout, "%-20s0x%08" PRIX32 "\n", "boot address:", rom_info.header.boot_address);
+    fprintf(stdout, "%-20s0x%08" PRIX32 "\n", "entrypoint:", rom_info.entrypoint);
+    fprintf(stdout, "%-20s0x%" PRIX32 "\n", "libultra:", rom_info.header.libultra_version);
+    fprintf(stdout, "%-20s0x%08" PRIX32"\n", "dmadata offset:", dmadata_info.offset);
+    fprintf(stdout, "%-20s%zu\n", "dmadata entries:", dmadata_info.count);
+    fprintf(stdout, "\n");
+
+    free(entries);
+    return ACTION_OK;
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,188 @@
+/*
+ * main.c: Program entrypoint
+ * Copyright (C) 2026 Jesse Gerard Brands
+ *
+ * This file is part of zelda64.
+ *
+ * zelda64 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * zelda64 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zelda64. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+#include <getopt.h>
+#include <zelda64/zelda64.h>
+
+#include "actions.h"
+
+#define EXIT_USAGE 2
+
+enum zelda64_option {
+    OPT_INFO = 'i',
+    OPT_VERBOSE = 'v',
+    OPT_VERSION = 256,
+    OPT_HELP = 257,
+};
+
+enum zelda64_action {
+    ACTION_INFO = 1u << 0,
+};
+
+enum zelda64_parse_result {
+    PARSE_OK,
+    PARSE_DONE,
+    PARSE_USAGE,
+};
+
+struct zelda64_options {
+    bool verbose;
+    char const* in_filename;
+    unsigned int actions;
+};
+
+static void
+show_usage(FILE* f) {
+    fprintf(f, "Usage: zelda64\n");
+    fprintf(f, "Manipulate Nintendo 64 Zelda ROMs.\n");
+    fprintf(f, "\n");
+    fprintf(f, "Options:\n");
+    fprintf(f, "  -v, --verbose        write verbose messages\n");
+    fprintf(f, "      --version        output version information\n");
+    fprintf(f, "      --help           show this help message\n");
+    fprintf(f, "\n");
+}
+
+static void
+print_version(FILE* f) {
+    fprintf(f, "zelda64 %s\n", zelda64_version_string());
+    fprintf(f, "Copyright (C) 2026  Jesse Gerard Brands\n");
+    fprintf(f, "This is free software; see the source for copying conditions.  There is NO\n");
+    fprintf(f, "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n");
+}
+
+static bool
+set_action(struct zelda64_options* options, enum zelda64_action const action) {
+    if (options->actions != 0 && !(options->actions & action)) {
+        return false;
+    }
+    options->actions |= action;
+    return true;
+}
+
+static enum zelda64_parse_result
+parse_options(struct zelda64_options* options, int argc, char** argv) {
+    bool show_help = false;
+    bool show_version = false;
+
+    struct option const long_options[] = {
+        {"info", no_argument, NULL, OPT_INFO},
+
+        {"verbose", no_argument, NULL, OPT_VERBOSE},
+        {"help", no_argument, NULL, OPT_HELP},
+        {"version", no_argument, NULL, OPT_VERSION},
+        {0, 0, 0, 0},
+    };
+
+    int c;
+
+    opterr = 0;
+    while ((c = getopt_long(argc, argv, ":vi", long_options, NULL)) != -1) {
+        switch (c) {
+            case OPT_VERBOSE:
+                options->verbose = true;
+                break;
+
+            case OPT_HELP:
+                show_help = true;
+                break;
+
+            case OPT_VERSION:
+                show_version = true;
+                break;
+
+            case OPT_INFO:
+                if (!set_action(options, ACTION_INFO)) {
+                    return EXIT_USAGE;
+                }
+                break;
+
+            case '?':
+                if (optopt != 0) {
+                    fprintf(stderr, "zelda64: error: unrecognized option '-%c'\n", optopt);
+                } else {
+                    fprintf(stderr, "zelda64: error: unrecognized option '%s'\n", argv[optind - 1]);
+                }
+                return PARSE_USAGE;
+
+            default:
+                abort();
+        }
+    }
+
+    if (show_help) {
+        show_usage(stdout);
+        return PARSE_DONE;
+    }
+
+    if (show_version) {
+        print_version(stdout);
+        return PARSE_DONE;
+    }
+
+    while (optind < argc) {
+        if (options->in_filename == NULL) {
+            options->in_filename = argv[optind++];
+            continue;
+        }
+        fprintf(stderr, "zelda64: error: too many arguments\n");
+        return EXIT_USAGE;
+    }
+
+    return PARSE_OK;
+}
+
 int main(int argc, char** argv) {
+    struct zelda64_options options = {0};
+    enum zelda64_parse_result const result = parse_options(&options, argc, argv);
+    switch (result) {
+        case PARSE_DONE:
+            return EXIT_SUCCESS;
+
+        case PARSE_USAGE:
+            fprintf(stderr, "See 'zelda64 --help' for usage information.\n\n");
+            return EXIT_USAGE;
+
+        case PARSE_OK:
+            break;
+    }
+
+    // Open file and get the file size.
+    FILE* in_file = fopen(options.in_filename, "rb");
+    fseek(in_file, 0, SEEK_END);
+    long const in_size = ftell(in_file);
+    rewind(in_file);
+
+    uint8_t* in_rom = malloc((size_t) in_size);
+    if (in_rom == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    fread(in_rom, 1, (size_t) in_size, in_file);
+
+    action_rom_info(in_rom, (size_t) in_size);
+
+    free(in_rom);
     return EXIT_SUCCESS;
 }
