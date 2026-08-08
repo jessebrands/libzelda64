2a5d4a5def6eb73354f755f1ffc922302345f222
Jesse Gerard Brands <jesse@jgbrands.nl>
2026-08-08 14:59:22 +0200

    Refactor of `decompress`

--- utils/decompress.c
+++ utils/decompress.c
@@ -18,76 +18,30 @@
  * along with zelda64. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <stdbool.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <yaz0/yaz0.h>
 #include <zelda64/zelda64.h>
 
-static inline void
-zelda64_write_u32(uint8_t* p, uint32_t const value) {
-    // @formatter:off
-    p[0] = (uint8_t) (value >> 24);
-    p[1] = (uint8_t) (value >> 16);
-    p[2] = (uint8_t) (value >> 8);
-    p[3] = (uint8_t) (value);
-    // @formatter:on
+#include "rom.h"
+
+static int
+print_error(enum zelda64_result const result, char const* what) {
+    fprintf(stderr, "decompress: error: %s: ", what);
+    if (result == ZELDA64_IO_ERROR) {
+        fprintf(stderr, "%s\n", strerror(errno));
+    } else {
+        fprintf(stderr, "%s\n", zelda64_result_string(result));
+    }
+    return EXIT_FAILURE;
 }
 
-static inline void
-zelda64_write_u64(uint8_t* p, uint64_t const value) {
-    zelda64_write_u32(&p[0], (uint32_t) (value >> 32));
-    zelda64_write_u32(&p[4], (uint32_t) (value));
-}
-
-static bool
-open_file(char const* filename, uint8_t** data, size_t* size) {
-    FILE* file = fopen(filename, "rb");
-    if (file == NULL) {
-        fprintf(stderr, "decompress: error: could not open file: %s\n", filename);
-        return false;
-    }
-
-    if (fseek(file, 0, SEEK_END) != 0) {
-        fprintf(stderr, "decompress: error: failed to read file: %s\n", filename);
-        goto cleanup_file;
-    }
-
-    long const file_size = ftell(file);
-    rewind(file);
-
-    if (file_size < 0) {
-        fprintf(stderr, "decompress: error: failed to get file size: %s\n", filename);
-        goto cleanup_file;;
-    }
-
-    *size = (size_t) file_size;
-    *data = malloc(*size);
-    if (*data == NULL) {
-        fprintf(stderr, "decompress: error: out of memory\n");
-        goto cleanup_file;
-    }
-
-    if (fread(*data, 1, *size, file) != *size) {
-        fprintf(stderr, "decompress: error: unexpected end of file: %s\n", filename);
-        goto cleanup_buffer;
-    }
-
-    fclose(file);
-    return true;
-
-cleanup_buffer:
-    free(*data);
-cleanup_file:
-    fclose(file);
-    *data = NULL;
-    *size = 0;
-    return false;
-}
 
 int main(int argc, char** argv) {
+    int exit_code = EXIT_SUCCESS;
+
     if (argc != 3) {
         fprintf(stderr, "Usage: %s rom outfile\n", argv[0]);
         return EXIT_FAILURE;
@@ -97,162 +51,73 @@ int main(int argc, char** argv) {
     char const* out_filename = argv[2];
 
     // Open the source ROM.
-    uint8_t* in_rom;
-    size_t in_size;
-    if (!open_file(in_filename, &in_rom, &in_size)) {
-        return EXIT_FAILURE;
-    }
-
-    struct zelda64_dmadata_info dma_info;
-    enum zelda64_result result = zelda64_find_dmadata(&dma_info, in_rom, in_size);
+    struct zelda64_rom in_rom;
+    enum zelda64_result result = zelda64_open_rom(in_filename, &in_rom);
     if (result != ZELDA64_OK) {
-        fprintf(stderr, "zelda64: error: could not find DMADATA: %s\n", zelda64_result_string(result));
-        return EXIT_FAILURE;
+        return print_error(result, "could not open input ROM");
     }
 
-    struct zelda64_dma_entry* in_table = calloc(dma_info.count, sizeof *in_table);
-    if (in_table == NULL) {
-        fprintf(stderr, "zelda64: error: out of memory!\n");
+    // Create the destination ROM.
+    struct zelda64_rom out_rom;
+    result = zelda64_create_rom(out_filename, DECOMPRESSED_SIZE, &in_rom.dma_info, &out_rom);
+    if (result != ZELDA64_OK) {
+        exit_code = print_error(result, "could not open output ROM");
         goto cleanup_in_rom;
     }
 
-    result = zelda64_read_dmadata(in_table, dma_info.count,
-                                  &in_rom[dma_info.offset], in_size - dma_info.offset);
-
-    if (result != ZELDA64_OK) {
-        fprintf(stderr, "zelda64: error: failed to read DMADATA: %s\n", zelda64_result_string(result));
-        goto cleanup_in_table;
-    }
-
-    // Allocate an output ROM.
-    uint8_t* out_rom = calloc(0x4000000, sizeof *out_rom);
-    if (out_rom == NULL) {
-        fprintf(stderr, "zelda64: error: out of memory!\n");
-        goto cleanup_in_table;
-    }
-
-    struct zelda64_dma_entry* out_table = calloc(dma_info.count, sizeof *in_table);
-    if (out_table == NULL) {
-        fprintf(stderr, "zelda64: error: out of memory!\n");
-        goto cleanup_out_rom;
-    }
-
-    for (size_t i = 0; i < dma_info.count; ++i) {
-        struct zelda64_dma_entry const in_entry = in_table[i];
+    for (size_t i = 0; i < in_rom.dma_info.count; ++i) {
+        struct zelda64_dma_entry const in_entry = in_rom.dma_table[i];
         enum zelda64_dma_kind const kind = zelda64_dma_entry_kind(&in_entry);
-        struct zelda64_dma_entry out_entry = in_entry;
 
         switch (kind) {
             case ZELDA64_DMA_EMPTY:
                 fprintf(stdout, "zelda64: skipping empty file %04zX\n", i);
+                out_rom.dma_table[i] = in_entry;
                 break;
 
             case ZELDA64_DMA_DELETED:
                 fprintf(stdout, "zelda64: skipping deleted file %04zX\n", i);
+                out_rom.dma_table[i] = in_entry;
                 break;
 
             case ZELDA64_DMA_UNCOMPRESSED: {
                 fprintf(stdout, "zelda64: copying file %04zX\n", i);
-
-                uint32_t file_offset = 0;
-                uint32_t file_size = 0;
-                result = zelda64_dma_entry_extent(&in_entry, &file_offset, &file_size);
+                result = zelda64_copy_file(&out_rom, &in_rom, i);
                 if (result != ZELDA64_OK) {
-                    fprintf(stderr, "zelda64: error: invalid entry: %s\n", zelda64_result_string(result));
-                    goto cleanup_out_table;
+                    exit_code = print_error(result, "failed to copy file");
+                    goto cleanup_out_rom;
                 }
-
-                memcpy(&out_rom[in_entry.vrom_start], &in_rom[file_offset], file_size);
-
-                // Fix up the table.
-                out_entry.rom_start = in_entry.vrom_start;
-                out_entry.rom_end = 0x0;
                 break;
             }
 
             case ZELDA64_DMA_COMPRESSED: {
                 fprintf(stdout, "zelda64: decompressing file %04zX\n", i);
-
-                uint32_t file_offset = 0;
-                uint32_t file_size = 0;
-                result = zelda64_dma_entry_extent(&in_entry, &file_offset, &file_size);
+                result = zelda64_decompress_file(&out_rom, &in_rom, i);
                 if (result != ZELDA64_OK) {
-                    fprintf(stderr, "zelda64: error: invalid entry: %s\n", zelda64_result_string(result));
-                    goto cleanup_out_table;
+                    exit_code = print_error(result, "failed to decompress file");
+                    goto cleanup_out_rom;
                 }
-
-                struct yaz0_stream stream = {0};
-                if (yaz0_decompress_init(&stream) != YAZ0_OK) {
-                    fprintf(stderr, "zelda64: error: could not initialize decompressor\n");
-                    goto cleanup_out_table;
-                }
-
-                stream.next_in = &in_rom[file_offset];
-                stream.avail_in = file_size;
-                stream.next_out = &out_rom[in_entry.vrom_start];
-                stream.avail_out = in_entry.vrom_end - in_entry.vrom_start;
-
-                enum yaz0_result const decomp_result = yaz0_decompress(&stream, YAZ0_FINISH);
-                if (decomp_result != YAZ0_STREAM_END) {
-                    fprintf(stderr, "zelda64: error: decompression failed: %s\n", yaz0_result_name(decomp_result));
-                    yaz0_decompress_end(&stream);
-                    goto cleanup_out_table;
-                }
-
-                yaz0_decompress_end(&stream);
-
-                // Fix up the table.
-                out_entry.rom_start = in_entry.vrom_start;
-                out_entry.rom_end = 0x0;
                 break;
             }
         }
-
-        out_table[i] = out_entry;
     }
 
     // Write out the fixed up table.
-    result = zelda64_write_dmadata(&out_rom[dma_info.offset], dma_info.size, out_table, dma_info.count);
+    result = zelda64_write_dmadata_to_rom(&out_rom);
     if (result != ZELDA64_OK) {
-        fprintf(stderr, "zelda64: error: failed to write DMADATA: %s\n", zelda64_result_string(result));
-        goto cleanup_out_table;
+        exit_code = print_error(result, "failed to write DMADATA");
+        goto cleanup_out_rom;
     }
 
-    // Recalculate the check code.
-    uint64_t check_code;
-    result = zelda64_rom_check_code(out_rom, 0x4000000, &check_code);
+    // Recalculate the check code and write out the header.
+    result = zelda64_finalize_rom(&out_rom);
     if (result != ZELDA64_OK) {
-        fprintf(stderr, "zelda64: error: failed to calculate check code: %s\n", zelda64_result_string(result));
-        goto cleanup_out_table;
+        exit_code = print_error(result, "failed to write ROM header");
     }
 
-    // TODO: This needs a library routine.
-    zelda64_write_u64(&out_rom[0x10], check_code);
-
-    // Write memory resident ROM to file.
-    FILE* out_file = fopen(out_filename, "wb");
-    if (out_file == NULL) {
-        fprintf(stderr, "zelda64: error: failed to open output ROM\n");
-        goto cleanup_out_table;
-    }
-
-    if (fwrite(out_rom, 1, 0x4000000, out_file) != 0x4000000) {
-        fprintf(stderr, "zelda64: error: did not write all bytes to output ROM file\n");
-        goto cleanup_out_file;
-    }
-
-    fclose(out_file);
-    return EXIT_SUCCESS;
-
-cleanup_out_file:
-    fclose(out_file);
-cleanup_out_table:
-    free(out_table);
 cleanup_out_rom:
-    free(out_rom);
-cleanup_in_table:
-    free(in_table);
+    zelda64_close_rom(&out_rom);
 cleanup_in_rom:
-    free(in_rom);
-    return EXIT_FAILURE;
+    zelda64_close_rom(&in_rom);
+    return exit_code;
 }
---
+++ utils/rom.c
@@ -0,0 +1,527 @@
+/*
+ * rom.c: ROM file operations
+ * Copyright (C) 2026 Jesse Gerard Brands
+ *
+ * This file is part of zelda64.
+ *
+ * zelda64 is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * zelda64 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zelda64. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <yaz0/yaz0.h>
+
+#include "rom.h"
+
+#define CHUNK_COUNT 128
+#define CHUNK_SIZE  (CHUNK_COUNT * ZELDA64_DMA_ENTRY_SIZE)
+
+static enum zelda64_result
+get_dma_info(struct zelda64_rom* rom) {
+    size_t offset = 0;
+    while (true) {
+        // Find the next read position.
+        if (fseek(rom->file, (long) offset, SEEK_SET) != 0) {
+            return ZELDA64_IO_ERROR;
+        }
+
+        // Read the next chunk of data.
+        uint8_t chunk[1024];
+        if (fread(chunk, sizeof chunk, 1, rom->file) != 1) {
+            return ZELDA64_IO_ERROR;
+        }
+
+        size_t seek_pos = 0;
+        while (zelda64_find_dmadata_start(chunk, sizeof chunk, &seek_pos) == ZELDA64_OK) {
+            // We found a position to look at, is this DMADATA?
+            size_t dma_start = offset + seek_pos;
+            if (fseek(rom->file, (long) dma_start, SEEK_SET) != 0) {
+                return ZELDA64_IO_ERROR;
+            }
+
+            uint8_t entries[3 * ZELDA64_DMA_ENTRY_SIZE];
+            if (fread(entries, ZELDA64_DMA_ENTRY_SIZE, 3, rom->file) != 3) {
+                return ZELDA64_IO_ERROR;
+            }
+
+            enum zelda64_result const result = zelda64_read_dmadata_info(
+                &rom->dma_info, dma_start, rom->file_size,
+                entries, sizeof entries
+            );
+
+            if (result == ZELDA64_OK) {
+                return result;
+            }
+
+            seek_pos += ZELDA64_DMA_ENTRY_SIZE;
+        }
+
+        offset += sizeof chunk;
+    }
+}
+
+static enum zelda64_result
+read_dmadata(struct zelda64_rom* rom) {
+    // Navigate to the start of the DMADATA
+    if (fseek(rom->file, rom->dma_info.offset, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    // Allocate a buffer large enough to hold our DMADATA
+    rom->dma_table = calloc(rom->dma_info.count, sizeof *rom->dma_table);
+    if (rom->dma_table == NULL) {
+        return ZELDA64_MEMORY_ERROR;
+    }
+
+    enum zelda64_result result = ZELDA64_OK;
+    for (size_t i = 0; i < rom->dma_info.count; i += CHUNK_COUNT) {
+        size_t const remaining = rom->dma_info.count - i;
+        size_t const want = remaining < CHUNK_COUNT ? remaining : CHUNK_COUNT;
+
+        uint8_t data[CHUNK_SIZE];
+        if (fread(data, ZELDA64_DMA_ENTRY_SIZE, want, rom->file) != want) {
+            result = ZELDA64_IO_ERROR;
+            goto cleanup_table;
+        }
+
+        result = zelda64_read_dmadata(&rom->dma_table[i], want, data, CHUNK_SIZE);
+        if (result != ZELDA64_OK) {
+            goto cleanup_table;
+        }
+    }
+
+    return ZELDA64_OK;
+
+cleanup_table:
+    free(rom->dma_table);
+    rom->dma_table = NULL;
+    return result;
+}
+
+static enum zelda64_result
+read_rom_info(struct zelda64_rom* rom) {
+    enum zelda64_result result = ZELDA64_OK;
+
+    uint32_t makerom_offset = 0;
+    uint32_t makerom_size = 0;
+    result = zelda64_dma_entry_extent(&rom->dma_table[0], &makerom_offset, &makerom_size);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // Allocate a temporary buffer for the MAKEROM
+    uint8_t* makerom = malloc(makerom_size);
+    if (makerom == NULL) {
+        return ZELDA64_MEMORY_ERROR;
+    }
+
+    if (fseek(rom->file, makerom_offset, SEEK_SET) != 0) {
+        result = ZELDA64_IO_ERROR;
+        goto cleanup_makerom;
+    }
+
+    if (fread(makerom, makerom_size, 1, rom->file) != 1) {
+        result = ZELDA64_IO_ERROR;
+        goto cleanup_makerom;
+    }
+
+    result = zelda64_read_rom_info(&rom->info, makerom, makerom_size);
+
+cleanup_makerom:
+    free(makerom);
+    return result;
+}
+
+enum zelda64_result
+zelda64_open_rom(char const* filename, struct zelda64_rom* rom) {
+    enum zelda64_result result = ZELDA64_OK;
+    if (filename == NULL || rom == NULL) {
+        return ZELDA64_INVALID_PARAMETER;
+    }
+
+    *rom = (struct zelda64_rom){0};
+
+    rom->filename = filename;
+    rom->file = fopen(rom->filename, "rb");
+    if (rom->file == NULL) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    if (fseek(rom->file, 0, SEEK_END) != 0) {
+        result = ZELDA64_IO_ERROR;
+        goto cleanup_file;
+    }
+
+    long const file_size = ftell(rom->file);
+    if (file_size < 0) {
+        result = ZELDA64_IO_ERROR;
+        goto cleanup_file;
+    }
+
+    rom->file_size = (size_t) file_size;
+    rewind(rom->file);
+
+    // Get the DMADATA info from the ROM.
+    result = get_dma_info(rom);
+    if (result != ZELDA64_OK) {
+        goto cleanup_file;
+    }
+
+    // Read the DMADATA to memory, we'll need it.
+    result = read_dmadata(rom);
+    if (result != ZELDA64_OK) {
+        goto cleanup_file;
+    }
+
+    result = read_rom_info(rom);
+    if (result != ZELDA64_OK) {
+        goto cleanup_table;
+    }
+
+    return ZELDA64_OK;
+
+cleanup_table:
+    free(rom->dma_table);
+    rom->dma_table = NULL;
+cleanup_file:
+    fclose(rom->file);
+    return result;
+}
+
+enum zelda64_result
+zelda64_create_rom(char const* filename, size_t const file_size,
+                   struct zelda64_dmadata_info const* dma_info,
+                   struct zelda64_rom* rom) {
+    if (filename == NULL || dma_info == NULL || rom == NULL) {
+        return ZELDA64_INVALID_PARAMETER;
+    }
+
+    enum zelda64_result result = ZELDA64_OK;
+
+    *rom = (struct zelda64_rom){0};
+
+    rom->filename = filename;
+    rom->file = fopen(rom->filename, "w+b");
+    rom->file_size = file_size;
+    if (rom->file == NULL) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    // Annoyingly, there's really no way for us to do this... =/
+    size_t bytes_out = 0;
+    while (bytes_out < rom->file_size) {
+        uint8_t const chunk[1024] = {0};
+        size_t const remaining = file_size - bytes_out;
+        size_t const have = remaining < sizeof chunk ? remaining : sizeof chunk;
+
+        if (fwrite(chunk, have, 1, rom->file) != 1) {
+            result = ZELDA64_IO_ERROR;
+            goto cleanup_file;
+        }
+
+        bytes_out += have;
+    }
+
+    // Allocate DMADATA for the ROM.
+    rom->dma_info = *dma_info;
+    rom->dma_table = calloc(rom->dma_info.count, sizeof *rom->dma_table);
+    if (rom->dma_table == NULL) {
+        result = ZELDA64_MEMORY_ERROR;
+        goto cleanup_file;
+    }
+
+    return ZELDA64_OK;
+
+cleanup_file:
+    fclose(rom->file);
+    rom->file = NULL;
+    return result;
+}
+
+void
+zelda64_close_rom(struct zelda64_rom* rom) {
+    fclose(rom->file);
+    free(rom->dma_table);
+    *rom = (struct zelda64_rom){0};
+}
+
+enum zelda64_result
+zelda64_copy_file(struct zelda64_rom* out_rom,
+                  struct zelda64_rom const* in_rom, size_t const file_index) {
+    if (out_rom == NULL || in_rom == NULL) {
+        return ZELDA64_INVALID_PARAMETER;
+    }
+
+    enum zelda64_result result = ZELDA64_OK;
+    struct zelda64_dma_entry const in_entry = in_rom->dma_table[file_index];
+
+    // We need to know the size of the file we're working with.
+    uint32_t file_offset = 0;
+    uint32_t file_size = 0;
+    result = zelda64_dma_entry_extent(&in_entry, &file_offset, &file_size);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // Set the read and write cursor of our ROMs.
+    if (fseek(out_rom->file, in_entry.vrom_start, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+    if (fseek(in_rom->file, file_offset, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    // Just a simple file copy loop from here on out.
+    size_t bytes_out = 0;
+
+    while (bytes_out < file_size) {
+        uint8_t chunk[1024];
+        size_t const remaining = file_size - bytes_out;
+        size_t const have = remaining < sizeof chunk ? remaining : sizeof chunk;
+
+        // Read input bytes.
+        if (fread(chunk, have, 1, in_rom->file) != 1) {
+            return ZELDA64_IO_ERROR;
+        }
+
+        // Write output bytes.
+        if (fwrite(chunk, have, 1, out_rom->file) != 1) {
+            return ZELDA64_IO_ERROR;
+        }
+
+        bytes_out += have;
+    }
+
+    // Update the DMA entry for the output ROM.
+    out_rom->dma_table[file_index] = (struct zelda64_dma_entry){
+        .vrom_start = in_entry.vrom_start,
+        .vrom_end = in_entry.vrom_end,
+        .rom_start = in_entry.vrom_start,
+        .rom_end = 0x0,
+    };
+
+    return ZELDA64_OK;
+}
+
+enum zelda64_result
+zelda64_decompress_file(struct zelda64_rom* out_rom,
+                        struct zelda64_rom const* in_rom, size_t const file_index) {
+    if (out_rom == NULL || in_rom == NULL) {
+        return ZELDA64_INVALID_PARAMETER;
+    }
+
+    enum zelda64_result result = ZELDA64_OK;
+    struct zelda64_dma_entry const in_entry = in_rom->dma_table[file_index];
+
+    // We need to know the size of the file we're working with.
+    uint32_t file_offset = 0;
+    uint32_t file_size = 0;
+    result = zelda64_dma_entry_extent(&in_entry, &file_offset, &file_size);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // Set the read and write cursor of our ROMs.
+    if (fseek(out_rom->file, in_entry.vrom_start, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+    if (fseek(in_rom->file, file_offset, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    // Initialize the decompressor.
+    struct yaz0_stream stream = {0};
+    if (yaz0_decompress_init(&stream)) {
+        return ZELDA64_DECOMPRESSION_ERROR;
+    }
+
+    enum yaz0_result decomp_result = YAZ0_OK;
+    enum yaz0_flush flush = YAZ0_NO_FLUSH;
+    uint8_t in_chunk[1024];
+    uint8_t out_chunk[1024];
+
+    size_t bytes_read = 0;
+
+    do {
+        size_t const remaining = file_size - bytes_read;
+        size_t const want = remaining < sizeof in_chunk ? remaining : sizeof in_chunk;
+
+        stream.avail_in = fread(in_chunk, 1, want, in_rom->file);
+        stream.next_in = in_chunk;
+        bytes_read += stream.avail_in;
+
+        flush = (bytes_read == file_size) ? YAZ0_FINISH : YAZ0_NO_FLUSH;
+
+        // Run the decompressor in a loop until we've received all output data.
+        do {
+            stream.next_out = out_chunk;
+            stream.avail_out = sizeof out_chunk;
+
+            decomp_result = yaz0_decompress(&stream, flush);
+            if (decomp_result < 0) {
+                yaz0_decompress_end(&stream);
+                return ZELDA64_DECOMPRESSION_ERROR;
+            }
+
+            // Write the decompressed bytes.
+            size_t const have = sizeof out_chunk - stream.avail_out;
+            if (fwrite(out_chunk, sizeof *out_chunk, have, out_rom->file) != have) {
+                yaz0_decompress_end(&stream);
+                return ZELDA64_IO_ERROR;
+            }
+        } while (stream.avail_out == 0 && stream.avail_in != 0);
+    } while (decomp_result != YAZ0_STREAM_END);
+
+    yaz0_decompress_end(&stream);
+
+    // Update the DMA entry for the output ROM.
+    out_rom->dma_table[file_index] = (struct zelda64_dma_entry){
+        .vrom_start = in_entry.vrom_start,
+        .vrom_end = in_entry.vrom_end,
+        .rom_start = in_entry.vrom_start,
+        .rom_end = 0x0,
+    };
+
+    return ZELDA64_OK;
+}
+
+enum zelda64_result
+zelda64_write_dmadata_to_rom(struct zelda64_rom* rom) {
+    if (rom == NULL) {
+        return ZELDA64_INVALID_PARAMETER;
+    }
+
+    // Navigate to the start of the DMADATA
+    if (fseek(rom->file, rom->dma_info.offset, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    enum zelda64_result result = ZELDA64_OK;
+    for (size_t i = 0; i < rom->dma_info.count; i += CHUNK_COUNT) {
+        size_t const remaining = rom->dma_info.count - i;
+        size_t const have = remaining < CHUNK_COUNT ? remaining : CHUNK_COUNT;
+
+        uint8_t data[CHUNK_SIZE];
+        result = zelda64_write_dmadata(data, CHUNK_SIZE, &rom->dma_table[i], have);
+        if (result != ZELDA64_OK) {
+            return result;
+        }
+
+        if (fwrite(data, ZELDA64_DMA_ENTRY_SIZE, have, rom->file) != have) {
+            return ZELDA64_IO_ERROR;
+        }
+    }
+
+    return ZELDA64_OK;
+}
+
+static enum zelda64_result
+calculate_rom_check_code(struct zelda64_rom* rom) {
+    // Refresh the ROM info, which is probably stale.
+    enum zelda64_result result = read_rom_info(rom);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // We need the IPL3 section for this.
+    // TODO: This smells of a library concern!
+    uint32_t const ipl_offset = 0x40;
+    uint32_t const ipl_size = 0xFC0;
+    uint8_t* ipl = malloc(ipl_size);
+    if (ipl == NULL) {
+        return ZELDA64_MEMORY_ERROR;
+    }
+
+    // Read the IPL3 into memory.
+    if (fseek(rom->file, ipl_offset, SEEK_SET) != 0) {
+        result = ZELDA64_IO_ERROR;
+        goto cleanup_ipl;
+    }
+
+    if (fread(ipl, ipl_size, 1, rom->file) != 1) {
+        result = ZELDA64_IO_ERROR;
+        goto cleanup_ipl;
+    }
+
+
+    struct zelda64_check_code_state state;
+    result = zelda64_cic_check_code_init(rom->info.cic, ipl, ipl_size, &state);
+    if (result != ZELDA64_OK) {
+        goto cleanup_ipl;
+    }
+
+    // TODO: Where does 0x101000 come from? The library of course.
+    while (state.offset < 0x101000) {
+        if (fseek(rom->file, (long) state.offset, SEEK_SET) != 0) {
+            result = ZELDA64_IO_ERROR;
+            goto cleanup_ipl;
+        }
+
+        uint8_t chunk[1024];
+        size_t const remaining = 0x101000 - state.offset;
+        size_t const want = remaining < sizeof chunk ? remaining : sizeof chunk;
+
+        if (fread(chunk, sizeof *chunk, want, rom->file) != want) {
+            result = ZELDA64_IO_ERROR;
+            goto cleanup_ipl;
+        }
+
+        result = zelda64_cic_check_code(&state, chunk, want);
+        if (result != ZELDA64_OK) {
+            goto cleanup_ipl;
+        }
+    }
+
+    uint64_t const check_code = zelda64_cic_check_code_end(&state);
+    rom->info.header.check_code = check_code;
+    result = ZELDA64_OK;
+
+cleanup_ipl:
+    free(ipl);
+    return result;
+}
+
+enum zelda64_result zelda64_finalize_rom(struct zelda64_rom* rom) {
+    enum zelda64_result result = calculate_rom_check_code(rom);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // Find the MAKEROM (it's always at offset 0, but just in case...)
+    uint32_t makerom_offset = 0;
+    uint32_t makerom_size = 0;
+    result = zelda64_dma_entry_extent(&rom->dma_table[0], &makerom_offset, &makerom_size);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // Write the new header out.
+    uint8_t header[ZELDA64_ROM_HEADER_SIZE];
+    result = zelda64_write_rom_header(header, ZELDA64_ROM_HEADER_SIZE, &rom->info.header);
+    if (result != ZELDA64_OK) {
+        return result;
+    }
+
+    // Write the new header to the MAKEROM.
+    if (fseek(rom->file, makerom_offset, SEEK_SET) != 0) {
+        return ZELDA64_IO_ERROR;
+    }
+    if (fwrite(header, ZELDA64_ROM_HEADER_SIZE, 1, rom->file) != 1) {
+        return ZELDA64_IO_ERROR;
+    }
+
+    return ZELDA64_OK;
+}
